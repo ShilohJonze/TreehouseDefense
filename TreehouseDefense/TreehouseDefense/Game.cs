9746767337606e57b87383c64ec4ab62872bd53d
Shiloh Mount <shilohjonze@gmail.com>
2016-10-17 12:55:46 -0400

    Added Tower and Game classes

---
+++ TreehouseDefense/TreehouseDefense/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        public void Main()
+        {
+            Tower tower = new Tower();
+        }
+    }
+}
